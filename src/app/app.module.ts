b102c2cc9aca04dbdd73334a962595c5a10ce32a
Pooja14206 <pooja.14206@gmail.com>
2021-08-23 10:25:05 +0530

    added a file for header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { AddComponent } from './component/add/add.component';
+// import { EmployeeDetailComponent } from './component/employee-detail/employee-detail.component';
+import { HomeComponent } from './component/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AddComponent,
+    // EmployeeDetailComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,

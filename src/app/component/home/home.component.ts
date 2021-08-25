b2905b81ff4d32fb4e73e24e15fe72de08faeefd
Pooja14206 <pooja.14206@gmail.com>
2021-08-25 12:14:23 +0530

    Added a file for connect rest call with Angular And Home page creation and design

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpService } from 'src/app/service/http.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public employeeCount: number = 10;
 
-  constructor() { }
-
+  constructor(private httpService: HttpService){}
   ngOnInit(): void {
+    this.httpService.getEmployeeData().subscribe(data=>console.log(data));
+
+
   }
 
 }
